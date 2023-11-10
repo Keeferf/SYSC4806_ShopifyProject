db7134436a1fe54cffc53ecd56b73e10e5b56d2f
isaiah <isaiahhunte1@gmail.com>
2023-11-09 22:16:30 -0500

    classes to be used for the project. (Maven)

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Product {
+    private String productName;
+    private String productDescription;
+    private int productInventory;
+
+    //image of the product
+}
---
+++ src/main/java/org/example/Shop.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class Shop {
+    private String shopName = null;
+    private String shopDescription = null;
+    private List<Product> products = null;
+
+    //Enum for the category possibly
+}
